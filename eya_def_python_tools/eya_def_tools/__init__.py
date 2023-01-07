d52cf5c6e7b43fec6aae786576c2030ca89d842c
christianj <christianj@naturalpower.com>
2023-01-07 05:31:35 +0100

    Update '__init__.py' files

--- eya_def_python_tools/eya_def_tools/__init__.py
+++ eya_def_python_tools/eya_def_tools/__init__.py
@@ -1,6 +1,7 @@
 """IEC 61400-15-2 EYA DEF toolset.
 
-The ``eya_def_tools`` module provides a toolset for working with the IEC
-61400-15-2 Energy Yield Assessment (EYA) Digital Exchange Format (DEF).
+The ``eya_def_tools`` package provides a toolset for working with the
+IEC 61400-15-2 Energy Yield Assessment (EYA) Digital Exchange Format
+(DEF) in Python.
 
 """
