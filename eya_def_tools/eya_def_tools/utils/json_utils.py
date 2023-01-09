baa2e1f84d521454d956c98bcd95d304a5d91e68
christianj <christianj@naturalpower.com>
2023-01-09 05:08:52 +0100

    Correct typos in docstrings

--- eya_def_tools/eya_def_tools/utils/json_utils.py
+++ eya_def_tools/eya_def_tools/utils/json_utils.py
@@ -4,10 +4,10 @@
 
 
 def reduce_json_all_of(json_dict: dict) -> dict:
-    """Get copy of JSON ``dict`` without superfluous ``ollOf``.
+    """Get copy of JSON ``dict`` without superfluous ``allOf``.
 
     :param json_dict: the original ``dict`` in JSON format
-    :return: a copy of ``json_dict`` where ``ollOf`` definitions are
+    :return: a copy of ``json_dict`` where ``allOf`` definitions are
         removed for instanced where there is only one item
     """
     reduced_json_dict = {}
