f5772ce7b7df85b332c6219a7cd1d805794ada4d
christianj <christianj@naturalpower.com>
2023-01-15 14:27:25 +0100

    Update placeholder 'reporting_engine'

--- eya_def_tools/eya_def_tools/reporting/reporting_engine.py
+++ eya_def_tools/eya_def_tools/reporting/reporting_engine.py
@@ -5,4 +5,33 @@ instances of the EYA DEF data model.
 
 """
 
-# TODO this is a placeholder module to be implemented
+from pathlib import Path
+
+import pandas as pd
+
+import eya_def_tools.data_models.energy_yield_assessment as eya
+from eya_def_tools.reporting import table_definitions
+
+
+class ReportingEngine:
+    """Engine for generating report tables from EYA DEF objects."""
+
+    def __int__(self, output_dirpath: Path, number_precision: int) -> None:
+        self.output_dirpath = output_dirpath
+        self.number_precision = number_precision
+
+    def generate_tables(
+        self, energy_yield_assessment: eya.EnergyYieldAssessment
+    ) -> dict[table_definitions.ReportingTableKey, pd.DataFrame]:
+        """Generate IEC 61400-15-2 tables from EYA DEF document.
+
+        :param energy_yield_assessment: the ``EnergyYieldAssessment``
+            instance to generate tables for
+        :return: a dictionary that maps table keys to pandas dataframe
+            objects with table data
+        """
+        tables: dict[table_definitions.ReportingTableKey, pd.DataFrame] = {}
+
+        # TODO placeholder function to be implemented
+
+        return tables
